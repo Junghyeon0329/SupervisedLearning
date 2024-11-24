594e7a069dc39498c7d2ebaab6e05df381831b0a
Junghyeon0329 <sptlsrja@gmail.com>
2024-11-24 19:01:43 +0900

    데이터 셋 변경

--- decision_tree.py
+++ decision_tree.py
@@ -8,11 +8,18 @@ from sklearn import tree
 import matplotlib.pyplot as plt
 
 # iris 데이터셋 로드
-iris = datasets.load_iris()
+# iris = datasets.load_iris()
 
 # 특징(Feature)와 레이블(Label) 분리
-X = iris.data
-y = iris.target
+# X = iris.data
+# y = iris.target
+
+df = pd.read_csv('otto_train.csv')
+
+# 2. 특징(Feature)와 레이블(Label) 분리
+# 여기서는 마지막 열이 레이블이라고 가정
+X = df.iloc[:, 1:-1].values  # 첫 번째 컬럼은 ID일 가능성이 높으므로 제외, 마지막 컬럼은 레이블
+y = df.iloc[:, -1].values  # 레이블은 마지막 컬럼
 
 # 데이터를 훈련용 데이터와 테스트용 데이터로 나눔 (80% 훈련, 20% 테스트)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
